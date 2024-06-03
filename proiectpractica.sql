ef8724401feb26aac16792c3a17105fc8ac92e9b
HolczliAndrei <116083078+HolczliAndrei@users.noreply.github.com>
2024-06-03 22:05:26 +0300

    Add files via upload

--- proiectpractica.sql
+++ proiectpractica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2024 at 05:52 PM
+-- Generation Time: Jun 03, 2024 at 09:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -74,7 +74,63 @@ INSERT INTO `answers` (`id`, `question_id`, `quiz_id`, `correct`, `answer_text`)
 (17, 12, 23, 1, 'Corect'),
 (18, 12, 23, 0, 'Gresit'),
 (19, 12, 23, 0, 'Gresit'),
-(20, 12, 23, 0, 'Gresit');
+(20, 12, 23, 0, 'Gresit'),
+(21, 13, 24, 1, 'Petric'),
+(22, 13, 24, 0, 'Mihalca'),
+(23, 13, 24, 0, 'Mihalca'),
+(24, 13, 24, 0, 'Mihalca'),
+(25, 14, 24, 1, 'Mihalca'),
+(26, 14, 24, 0, 'Petric'),
+(27, 14, 24, 0, 'Andrei'),
+(28, 14, 24, 0, 'Andrei si Petric'),
+(29, 15, 24, 1, '10'),
+(30, 15, 24, 0, '1'),
+(31, 15, 24, 0, '4'),
+(32, 15, 24, 0, '7'),
+(33, 16, 24, 1, 'Andrei'),
+(34, 16, 24, 0, 'Petric'),
+(35, 16, 24, 0, 'Mihalca'),
+(36, 16, 24, 0, 'Petrus'),
+(37, 17, 24, 1, 'Copalnic'),
+(38, 17, 24, 0, 'Baia Mare'),
+(39, 17, 24, 0, 'Paris'),
+(40, 17, 24, 0, 'Wildwood'),
+(41, 18, 24, 1, 'Andrei'),
+(42, 18, 24, 0, 'Petric'),
+(43, 18, 24, 0, 'Mihalca'),
+(44, 18, 24, 0, 'Petrus'),
+(45, 19, 25, 1, 'Eu'),
+(46, 19, 25, 0, 'EU'),
+(47, 19, 25, 0, 'EeU'),
+(48, 19, 25, 0, 'EE'),
+(49, 20, 25, 1, 'TU'),
+(50, 20, 25, 0, 'tu'),
+(51, 20, 25, 0, 'tU'),
+(52, 20, 25, 0, 'Ut'),
+(53, 21, 26, 1, 'EA'),
+(54, 21, 26, 0, 'EA'),
+(55, 21, 26, 0, 'A'),
+(56, 21, 26, 0, 'AA'),
+(57, 22, 26, 1, '44'),
+(58, 22, 26, 0, '11'),
+(59, 22, 26, 0, '22'),
+(60, 22, 26, 0, '22'),
+(61, 23, 28, 1, 'DA'),
+(62, 23, 28, 0, ''),
+(63, 23, 28, 0, 'dad'),
+(64, 23, 28, 0, 'sada'),
+(65, 24, 28, 1, 'ASAZ'),
+(66, 24, 28, 0, 'QWQET'),
+(67, 24, 28, 0, 'DADA'),
+(68, 24, 28, 0, 'EHH'),
+(69, 25, 29, 1, 'azcs'),
+(70, 25, 29, 0, 'adbt'),
+(71, 25, 29, 0, 'adawq'),
+(72, 25, 29, 0, 'tdds'),
+(73, 26, 29, 1, 'ARA'),
+(74, 26, 29, 0, 'adsd'),
+(75, 26, 29, 0, 'casc'),
+(76, 26, 29, 0, 'gttr');
 
 -- --------------------------------------------------------
 
@@ -116,7 +172,14 @@ INSERT INTO `exam_results` (`id`, `email`, `exam_name`, `total_question`, `corre
 (4, 'Darius', 'Quiz Andrei', '2', '2', '0', '2024-06-03'),
 (5, 'Darius', 'Quiz Andrei', '2', '1', '1', '2024-06-03'),
 (6, 'Darius', 'Quiz Andrei', '2', '2', '0', '2024-06-03'),
-(7, 'Darius', 'Quiz Andrei', '2', '2', '0', '2024-06-03');
+(7, 'Darius', 'Quiz Andrei', '2', '2', '0', '2024-06-03'),
+(8, 'Darius', 'Quiz Andrei', '2', '2', '0', '2024-06-03'),
+(9, 'Darius', 'Quiz Andrei', '2', '2', '0', '2024-06-03'),
+(10, 'Darius', 'Quiz Andrei', '2', '1', '1', '2024-06-03'),
+(11, 'Darius', 'Quiz Andrei', '2', '0', '2', '2024-06-03'),
+(12, 'Darius', 'Quiz Andrei', '2', '0', '2', '2024-06-03'),
+(13, 'Darius', 'Quiz Complet', '6', '6', '0', '2024-06-03'),
+(14, 'Darius', 'Quiz test 5', '2', '0', '2', '2024-06-03');
 
 -- --------------------------------------------------------
 
@@ -139,7 +202,21 @@ INSERT INTO `questions` (`id`, `question_text`, `question_no`, `quiz_id`) VALUES
 (9, ' Care dintre următoarele opțiuni reprezintă un tip de bază de date relațională?', 1, 21),
 (10, 'Care este capitala frantei?', 1, 22),
 (11, 'Care este capitala Romaniei?', 2, 22),
-(12, 'Intrebarea 1', 1, 23);
+(12, 'Intrebarea 1', 1, 23),
+(13, 'Cine a facut cel mai mult la proiect', 1, 24),
+(14, 'Cine a facut cel mai putin la proiect?', 2, 24),
+(15, 'Ce nota luam?', 3, 24),
+(16, 'Cine a creeat acest quiz?', 4, 24),
+(17, 'In ce localitate sta mihalca?', 5, 24),
+(18, 'Cine are cea mai tare masina?', 6, 24),
+(19, 'Cine', 1, 25),
+(20, 'tu', 1, 25),
+(21, 'Prima iNTREBARE', 1, 26),
+(22, 'A DOUA INTREBARE', 1, 26),
+(23, 'dada', 1, 28),
+(24, 'DADRR', 2, 28),
+(25, 'acvx', 1, 29),
+(26, 'dDDSDA', 2, 29);
 
 -- --------------------------------------------------------
 
@@ -173,7 +250,14 @@ INSERT INTO `quizez` (`id`, `name`, `description`, `picture`, `time`) VALUES
 (20, 'Database', 'test', '665b1fea31f6d.jpg', 0),
 (21, 'Database', 'test', '665b3f6343b7c.jpg', 2),
 (22, 'Quiz Andrei', 'Quiz ', '665c87110b72f.png', 1),
-(23, 'Quiz test', 'test', '665c8c37eb314.png', 3);
+(23, 'Quiz test', 'test', '665c8c37eb314.png', 3),
+(24, 'Quiz Complet', 'Primul quiz complet', '665dffc044621.png', 0),
+(25, 'Quiz test 2', 'Quiz test 2', '665e040980284.png', 0),
+(26, 'Quit test 3', 'Quiz test 3', '665e04837ccb2.png', 0),
+(27, 'mOR', 'NMO', '665e04ef993c0.png', 0),
+(28, 'Quiz test 4', 'Quiz test 4', '665e0938095e3.png', 0),
+(29, 'Quiz test 5', 'Quiz test 5', '665e0a308c441.png', 0),
+(30, 'AAA', 'BBB', '665e11082835d.png', 0);
 
 -- --------------------------------------------------------
 
@@ -263,7 +347,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT for table `conturi`
@@ -275,19 +359,19 @@ ALTER TABLE `conturi`
 -- AUTO_INCREMENT for table `exam_results`
 --
 ALTER TABLE `exam_results`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `quizez`
 --
 ALTER TABLE `quizez`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `registration`
